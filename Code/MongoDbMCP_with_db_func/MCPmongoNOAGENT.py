c4e73bb7ebfb1b7f9aad689355ccd319cfb0395b
Tayyaba Arooj R <aroojtayyaba.r@gmail.com>
2025-05-17 20:43:40 +0500

    Update MCPmongoNOAGENT.py

--- Code/MongoDbMCP_with_db_func/MCPmongoNOAGENT.py
+++ Code/MongoDbMCP_with_db_func/MCPmongoNOAGENT.py
@@ -1,15 +1,15 @@
 from mcp.server.fastmcp import FastMCP
 from pymongo import MongoClient
 
-# Initialize FastMCP (for tool registration)
+
 mcp = FastMCP("IceCreamRevenue")
 
-# Connect to MongoDB
+
 client = MongoClient("mongodb://localhost:27017/")
 db = client["mcppracdb"]
 collection = db["newcol"]
 
-# Tool to query revenue by temperature
+
 @mcp.tool()
 def query_revenue_by_temperature(Temperature: float) -> dict:
     """
@@ -17,7 +17,7 @@ def query_revenue_by_temperature(Temperature: float) -> dict:
     """
     return collection.find_one({"Temperature": Temperature}) or {"message": f"No document found for temperature {Temperature}."}
 
-# Tool to update revenue for a specific temperature
+
 @mcp.tool()
 def update_revenue(Temperature: float, new_revenue: float) -> dict:
     """
@@ -38,7 +38,7 @@ def delete_revenue(Temperature: float) -> dict:
     result = collection.delete_one({"Temperature": Temperature})
     return {"deleted_count": result.deleted_count}
 
-# Tool to create an index on temperature
+
 @mcp.tool()
 def create_temperature_index() -> dict:
     """
